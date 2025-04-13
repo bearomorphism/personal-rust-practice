4b03200694300b2f1f1cc55776fec6f0f3126407
Tim Hsiung <bear890707@gmail.com>
2025-04-13 23:15:04 +0800

    WIP

--- main.rs
+++ main.rs
@@ -1,3 +1,28 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
     println!("Hello, world!");
+
+    let secret_number = rand::rng().random_range(1..=100);
+
+    #[allow(while_true)]
+    while true {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
+    println!("The secret number was: {}", secret_number);
 }
