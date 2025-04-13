3d89eea2359481a9ac6e2a67bd3907b031363d02
Tim Hsiung <bear890707@gmail.com>
2025-04-13 23:22:46 +0800

    WIP

--- main.rs
+++ main.rs
@@ -7,14 +7,20 @@ fn main() {
 
     let secret_number = rand::rng().random_range(1..=100);
 
-    #[allow(while_true)]
-    while true {
+    loop {
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number!");
+                continue;
+            }
+        };
+
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
